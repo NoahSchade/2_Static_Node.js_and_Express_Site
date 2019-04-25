8c92880c5843576b2d2104ab75eb7cecd8ceeaac
Noah Schade <noah9@mail.com>
2019-04-25 15:21:28 -0400

    Update code

--- public/js/style_2.js
+++
@@ -1,8 +0,0 @@
-// $(".off-canvas.position-left.reveal-for-large.portfolio-me.is-transition-push").mouseleave(function(){
-//     $(".off-canvas.position-left.reveal-for-large.portfolio-me.is-transition-push").css("background-color", "rgba(0, 0, 0, 0.2)");
-//     $(".off-canvas.position-left.reveal-for-large.portfolio-me.is-transition-push").css("transition", "background-color 2s ease");
-// });
-
-// $(".off-canvas.position-left.reveal-for-large.portfolio-me.is-transition-push").mouseenter(function(){
-//     $(".off-canvas.position-left.reveal-for-large.portfolio-me.is-transition-push").css("background-color", "rgba(0, 0, 0, 1)");
-// });
